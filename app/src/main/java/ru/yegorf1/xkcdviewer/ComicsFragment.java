6b5c0255aad1a1bd41adb03f4672239f509adef1
yegorf1 <yegorf1@gmail.com>
2015-06-01 11:54:30 +0300

    Wrote main stuff of ComicsFragment class

--- app/src/main/java/ru/yegorf1/xkcdviewer/ComicsFragment.java
+++ app/src/main/java/ru/yegorf1/xkcdviewer/ComicsFragment.java
@@ -1,17 +1,25 @@
 package ru.yegorf1.xkcdviewer;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import uk.co.senab.photoview.PhotoViewAttacher;
 
 
 public class ComicsFragment extends Fragment {
     private static final String ARG_COMICS_NUMBER = "number";
 
     private int comicsNumber;
+    private TextView comicsTitleTextView;
+    private ImageView comicsImageView;
+    private PhotoViewAttacher comicsImageAttacher;
 
     public static ComicsFragment newInstance(int comicsNumber) {
         ComicsFragment fragment = new ComicsFragment();
@@ -28,6 +36,7 @@ public class ComicsFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         if (getArguments() != null) {
             comicsNumber = getArguments().getInt(ARG_COMICS_NUMBER);
         }
@@ -36,7 +45,16 @@ public class ComicsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_comics, container, false);
+        View v = inflater.inflate(R.layout.fragment_comics, container, false);
+
+        comicsTitleTextView = (TextView)v.findViewById(R.id.comics_title);
+        comicsImageView = (ImageView)v.findViewById(R.id.comics_image);
+
+        new DownloadComicsTask(this).execute(comicsNumber);
+
+        comicsImageAttacher = new PhotoViewAttacher(comicsImageView);
+
+        return v;
     }
 
     @Override
@@ -48,4 +66,29 @@ public class ComicsFragment extends Fragment {
     public void onDetach() {
         super.onDetach();
     }
+
+    private void setComics(XkcdAPI.ComicsInfo result) {
+        comicsTitleTextView.setText(result.title);
+        new DownloadImageTask(comicsImageView).execute(result.imageUrl);
+    }
+
+    private class DownloadComicsTask extends AsyncTask<Integer, Void, XkcdAPI.ComicsInfo> {
+        private ComicsFragment fragment;
+
+        public DownloadComicsTask(ComicsFragment fragment) {
+            this.fragment = fragment;
+        }
+
+        @Override
+        protected XkcdAPI.ComicsInfo doInBackground(Integer... params) {
+            int id = params[0];
+
+            return XkcdAPI.getComicsByID(id);
+        }
+
+        protected void onPostExecute(XkcdAPI.ComicsInfo result) {
+            fragment.setComics(result);
+        }
+    }
 }
+
