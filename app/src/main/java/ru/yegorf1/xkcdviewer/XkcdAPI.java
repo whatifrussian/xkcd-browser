053f2dde284a938514ff857f4c9392da1cb011dd
yegorf1 <yegorf1@gmail.com>
2015-06-01 14:47:49 +0300

    Implemented urlToId, getComicsList, getLastComicsUrl methods

--- app/src/main/java/ru/yegorf1/xkcdviewer/XkcdAPI.java
+++ app/src/main/java/ru/yegorf1/xkcdviewer/XkcdAPI.java
@@ -4,6 +4,7 @@ import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -11,16 +12,22 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class XkcdAPI {
-    public static class ComicsInfo {
+    public static class BaseComicsInfo {
+        public int id;
         public String title;
-        public String imageUrl;
         public String thumbnailUrl;
+        public String url;
+    }
+
+    public static class ComicsInfo extends BaseComicsInfo {
+        public String imageUrl;
         public String text;
         public String comment;
         public String originalUrl;
-        public String url;
         public String next;
         public String previous;
         public String first;
@@ -30,8 +37,13 @@ public class XkcdAPI {
         public ComicsInfo() { }
     }
 
+    private static ComicsInfo lastLoaded;
+
     public static ComicsInfo getComicsByID(int id) {
-        return  getComicsByURL("http://xkcd.ru/" + id + "/");
+        ComicsInfo info = getComicsByURL("http://xkcd.ru/" + id + "/");
+        info.id = id;
+
+        return info;
     }
 
     public static ComicsInfo getComicsByURL(String url) {
@@ -41,6 +53,7 @@ public class XkcdAPI {
         try {
             json = getPageSource(jsonUrl);
         } catch (IOException ex) {
+            lastLoaded = new ComicsInfo();
             return new ComicsInfo();
         }
 
@@ -64,10 +77,78 @@ public class XkcdAPI {
             res.last = jsonObj.getString("last");
             res.random = jsonObj.getString("random");
 
+            lastLoaded = res;
+
             return res;
         } catch (JSONException ignored) { }
 
+        lastLoaded = new ComicsInfo();
+
         return new ComicsInfo();
+    }
+
+    public static String getLastComicsURL() {
+        if (lastLoaded == null || lastLoaded.equals(new ComicsInfo())) {
+            List<BaseComicsInfo> infos = getComicsList();
+            return getComicsByURL(infos.get(infos.size() - 1).url).url;
+        } else {
+            return lastLoaded.last;
+        }
+    }
+
+    public static int getLastComicsId() {
+        return urlToId(getLastComicsURL());
+    }
+
+    public static int getLastComicsId(boolean canUseInternet) {
+        if (canUseInternet) {
+            return  getLastComicsId();
+        } else if (lastLoaded != null) {
+            return urlToId(lastLoaded.last);
+        } else {
+            return 0;
+        }
+    }
+
+
+    public static int urlToId(String url) {
+        return Integer.parseInt(url.replaceAll("\\D+",""));
+    }
+
+    public static List<BaseComicsInfo> getComicsList() {
+        List<BaseComicsInfo> res = new ArrayList<>();
+
+        String jsonUrl = "http://xkcd.ru/num/?json=1";
+        String json;
+
+        try {
+            json = getPageSource(jsonUrl);
+        } catch (IOException ex) {
+            return res;
+        }
+
+        try {
+            JSONObject jsonObj = new JSONObject(json);
+            JSONArray list = jsonObj.getJSONArray("list");
+
+            for (int i = 0; i < list.length(); i++) {
+                try {
+                    JSONObject jsonInfo = list.getJSONObject(i);
+
+                    BaseComicsInfo info = new BaseComicsInfo();
+
+                    info.id = jsonInfo.getInt("id");
+                    info.title = jsonInfo.getString("title");
+                    info.thumbnailUrl = jsonInfo.getString("thumbnail");
+                    info.url = jsonInfo.getString("url");
+
+                    res.add(info);
+                } catch (JSONException ignored) { }
+            }
+
+        } catch (JSONException ignored) { }
+
+        return res;
     }
 
     private static String getPageSource(String url) throws IOException {
