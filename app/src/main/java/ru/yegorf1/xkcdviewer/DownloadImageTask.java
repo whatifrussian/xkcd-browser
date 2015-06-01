2a2d52ffad01d6aedf370aa8f98eb84248d2291a
yegorf1 <yegorf1@gmail.com>
2015-06-01 14:46:40 +0300

    Added PhotoView spport for download task

--- app/src/main/java/ru/yegorf1/xkcdviewer/DownloadImageTask.java
+++ app/src/main/java/ru/yegorf1/xkcdviewer/DownloadImageTask.java
@@ -8,11 +8,18 @@ import android.widget.ImageView;
 
 import java.io.InputStream;
 
+import uk.co.senab.photoview.PhotoViewAttacher;
+
 public class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
     private ImageView bmImage;
+    private PhotoViewAttacher attacher;
 
     public DownloadImageTask(ImageView bmImage) {
         this.bmImage = bmImage;
+    }
+    public DownloadImageTask(ImageView bmImage, PhotoViewAttacher attacher) {
+        this.bmImage = bmImage;
+        this.attacher = attacher;
     }
 
     protected Bitmap doInBackground(String... urls) {
@@ -31,5 +38,8 @@ public class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
 
     protected void onPostExecute(Bitmap result) {
         bmImage.setImageBitmap(result);
+        if (attacher != null) {
+            attacher.update();
+        }
     }
 }
