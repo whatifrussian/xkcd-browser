4d4af7694c1249d394d223981171966e243fa0dd
yegorf1 <yegorf1@gmail.com>
2015-06-01 11:49:21 +0300

    Added DownloadImageTask

---
+++ app/src/main/java/ru/yegorf1/xkcdviewer/DownloadImageTask.java
@@ -0,0 +1,35 @@
+package ru.yegorf1.xkcdviewer;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.ImageView;
+
+import java.io.InputStream;
+
+public class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+    private ImageView bmImage;
+
+    public DownloadImageTask(ImageView bmImage) {
+        this.bmImage = bmImage;
+    }
+
+    protected Bitmap doInBackground(String... urls) {
+        String urldisplay = urls[0];
+        Bitmap mIcon11 = null;
+        InputStream in;
+        try {
+            in = new java.net.URL(urldisplay).openStream();
+            mIcon11 = BitmapFactory.decodeStream(in);
+            in.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return mIcon11;
+    }
+
+    protected void onPostExecute(Bitmap result) {
+        bmImage.setImageBitmap(result);
+    }
+}
