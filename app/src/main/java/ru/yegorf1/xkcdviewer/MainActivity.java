b1accbfb375dff1d88d85c9ad7366b77f0d7d845
yegorf1 <yegorf1@gmail.com>
2015-08-21 10:37:14 +0300

    Added PhotoViewLibrary localy

--- app/src/main/java/ru/yegorf1/xkcdviewer/MainActivity.java
+++ app/src/main/java/ru/yegorf1/xkcdviewer/MainActivity.java
@@ -66,7 +66,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onLongPress() {
                 if (currentFragment == null || currentFragment.comicsInfo == null) { return; }
-                
+
                 String text = currentFragment.comicsInfo.text;
                 AlertDialog.Builder dlgAlert  = new AlertDialog.Builder(MainActivity.this);
                 dlgAlert.setMessage(text);
